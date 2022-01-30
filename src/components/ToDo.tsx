cab5dc855ab1e0158c3bfa017253178985f38eb7
HyesuHong <honghs95@gmail.com>
2022-01-31 02:08:21 +0900

    add function - change to do state

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,41 @@
-import { IToDos } from '../atoms';
+import React from 'react';
+import { useSetRecoilState } from 'recoil';
+import { IToDos, toDoState } from '../atoms';
 
-function ToDo({ text }: IToDos) {
+function ToDo({ text, category, id }: IToDos) {
+	const setToDos = useSetRecoilState(toDoState);
+	const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		const {
+			currentTarget: { name },
+		} = event;
+		setToDos((prev) => {
+			const targetIndex = prev.findIndex((todo) => todo.id === id);
+			const newToDo = { text: text, id: id, category: name as any };
+			return [
+				...prev.slice(0, targetIndex),
+				newToDo,
+				...prev.slice(targetIndex + 1),
+			];
+		});
+	};
 	return (
 		<li>
 			<span>{text}</span>
-			<button>Done</button>
-			<button>Doing</button>
-			<button>To Do</button>
+			{category !== 'DONE' && (
+				<button onClick={onClick} name='DONE'>
+					Done
+				</button>
+			)}
+			{category !== 'DOING' && (
+				<button onClick={onClick} name='DOING'>
+					Doing
+				</button>
+			)}
+			{category !== 'TO_DO' && (
+				<button onClick={onClick} name='TO_DO'>
+					To Do
+				</button>
+			)}
 		</li>
 	);
 }
